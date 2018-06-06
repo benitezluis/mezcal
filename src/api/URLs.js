9a0f8fa1e50a9ab37104325ecb960008af67c30c
Luis <luisbenitezpd@gmail.com>
2018-06-06 10:03:30 -0500

    login integration azure

---
+++ src/api/URLs.js
@@ -0,0 +1,6 @@
+export const BASE_URL_DEV = 'http://localhost:3000';
+export const BASE_URL_PROD = 'http://google.com';
+export const LOGIN_AZURE = '';
+
+// Internal routes
+export const URL_LOGIN = 'login';
